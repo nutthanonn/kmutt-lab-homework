b95cf84ce75a586a06fbea8f5bfb66ce3e6fb8e8
nutthanon <nutthanon.tho@gmail.com>
2021-09-07 14:23:43 +0700

    [+] Lab

--- lab3/01.py
+++ lab3/01.py
@@ -1,10 +1,14 @@
-"""
-TESTCASE!!!!
+max = 8
+n = 6
+check = max
+# 8, 6
 
 
 
-
-"""
-
-
-
+for i in range(1, max + 1):
+    print("Multiplication table of " + str(i))
+    if i == n:
+        check = n
+    for j in range(1, check + 1):
+       print(i * j, end=" ")
+    print()
--- lab3/03.py
+++ lab3/03.py
@@ -1,8 +1,42 @@
-"""
-TESTCASE!!!!
+import random
+import math
+
+name = []
+checkVincent = []
+
+for i in range(1, 1001):
+    if (i  % 100) == 0:
+        print("Xingqui : " + str(name.count("Xingqui")))
+        print("Raiden : " + str(name.count("Raiden")))
+        print("Eula  : " + str(name.count("Eula")))
+        print("Riktor : " + str(name.count("Riktor")))
+        print("Aleister : " + str(name.count("Aleister")))
+        print("Vincent : " + str(name.count("Vincent")))
+        print("----------")
+        name.clear()
+    
+    if len(checkVincent) == 350:
+            print("Vincent more than 350")
+            break
+    
+    ra = math.floor(random.random() * 100)
+
+    if 0 <= ra <= 10:
+        name.append("Xingqui")
+
+    elif 10 < ra <= 35:
+        check = ['Eula', 'Raiden']
+        r = math.floor(random.random() * 2)
+        name.append(check[r])
+
+    elif 35 < ra <= 65:
+        check = ['Aleister', 'Riktor']
+        r = math.floor(random.random() * 2)
+        name.append(check[r])
+
+    elif 65 < ra <= 100:
+        name.append("Vincent")
+        checkVincent.append("Vincent")
 
 
-
-
-
-"""
+print(len(checkVincent))
