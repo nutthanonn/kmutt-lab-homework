f55fb1a15b0b65001aeedfa7353543b5698a8e91
nutthanon <nutthanon.tho@gmail.com>
2021-10-19 16:11:13 +0700

    [+] lab

--- lab5/02.py
+++ lab5/02.py
@@ -6,10 +6,12 @@ import math
 def trigonometry(x):
     s = "%.15f" % math.sin(math.radians(x))
     c = "%.15f" % math.cos(math.radians(x))
-    return s, c
+    return [s, c]
 
 a = 0
 for i in range(int(360/22.5)+1):
-    print(a, trigonometry(a))
+    print()
+    print(str(a), "     sin ->", trigonometry(a)[0], "    cos ->", trigonometry(a)[1])
     a += 22.5
+    print()
     print("<------------------------------------------------------------------------->")
