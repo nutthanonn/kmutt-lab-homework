6f9db2ba3c3efa6e23866066cb5d180e5dac30dc
nutthanon <nutthanon.tho@gmail.com>
2021-11-02 13:29:09 +0700

    [+] lab

--- lab7/02.py
+++ lab7/02.py
@@ -1,10 +1,11 @@
+import uuid 
 l = []
-VALUE = list()
-def check(paramiter = VALUE):
-    if paramiter == VALUE:
+UNIQUE_ID = uuid.uuid4() #generate uuidV4 ถ้าจารไม่ให้ใช้เปลี่ยนเป็น -> UNIQUE_ID = list()
+def check(paramiter = UNIQUE_ID):
+    if paramiter == UNIQUE_ID:
        return "Invalid Number"
     for i in paramiter:
-        if  48 <= ord(i) <= 57 and i in l: 
+        if  48 <= ord(i) <= 57 and i in l:  # ord เป็นการเปลี่ยน ตัวอักษรให้อยู่ในเลข ASCII Table โดย รหัส Ascii 0-9 คือ ช่วงของ 48-57
             return i
         l.append(i)
 
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-DEFAULT = object()
-def foo(param=DEFAULT):
-    if param is DEFAULT:
-        print("Not paramiter")
-    else:
-        print("Hello")
-foo()
-foo("")
+l = []
+UNIQUE_ID = list()
+def check(paramiter = UNIQUE_ID):
+    if paramiter == UNIQUE_ID:
+       return "Invalid Number"
+    for i in paramiter:
+        if  48 <= ord(i) <= 57 and i in l: 
+            return i
+        l.append(i)
+
+print(check('ab138g579b'))
+print(check('h54325b1'))
+print(check())
