1f88c3fe666559fa5c1e9f65a189dce0ebf0bff2
nutthanon <nutthanon.tho@gmail.com>
2022-02-24 14:54:05 +0700

    lab5

--- com-programming/lab5/86_7.py
+++ com-programming/lab5/86_7.py
@@ -1,21 +1,23 @@
-
 """
 TEST CASE
 10
 
 """
-
 n = int(input())
-list_x = []
-for i in range(2, n-1):
-    for j in range(2, n//2-1):
-        list_x.append(i*j)
+list_c = []
+for i in range(2, n):
+    x = []
+    for j in range(2, n):
+        if i*j < n:
+            x.append(i*j)
+    list_c.append(x)
 
+
+list_x = [int(y) for x in list_c for y in x]
 list_x.sort()
 new_list = []
 for i in range(len(list_x) - 1):
     if list_x[i] != list_x[i+1]:
         new_list.append(list_x[i])
 new_list.append(list_x[-1])
-
 print(new_list)
