7209bd2699e6c0912dbffc3f7217bd4d4802fb67
nutthanon <nutthanon.tho@gmail.com>
2022-03-03 10:13:30 +0700

    [+] lab6

---
+++ com-programming/lab6/103_1.py
@@ -0,0 +1,24 @@
+"""
+TESTCASE
+
+5
+Tom Engineering
+Pam Arts
+Jim Engineering
+Tom Arts
+Jenny Science
+Engineering Arts
+
+
+"""
+
+n = int(input())
+name_position = []
+name = set()
+for i in range(n):
+    name_position.append(input().split())
+word_name = [x for x in input().split()]
+for i in name_position:
+    if i[1] in word_name:
+        name.add(i[0])
+print(*name)
